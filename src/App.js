dccc084441397b50c97b310c477537b38d87869f
Khan <sameer_khan@optum.com>
2025-01-22 20:27:18 +0530

    added router

--- src/App.js
+++ src/App.js
@@ -1,9 +1,14 @@
 import { useState } from 'react';
 import './App.css';
-// import About from './components/About';
+import About from './components/About';
 import Navbar from './components/Navbar';
 import TextForm from './components/TextForm';
 import Alert from './components/Alert';
+import {
+  BrowserRouter as Router,
+  Routes,
+  Route
+} from "react-router-dom";
 
 function App() {
   const [mode, setMode] = useState('dark blue')
@@ -24,6 +29,12 @@ function App() {
       setMode('dark')
       document.body.style.backgroundColor = '#042743';
       showAlert("Dark Mode has unabled", "success")
+      // setInterval(() => {
+      //   document.title = "TextUtils - Dark Mode"
+      // }, 500);
+      // setInterval(() => {
+      //   document.title = "TextUtils - Light Mode"
+      // }, 500);
     }else{
       setMode('light')
       document.body.style.backgroundColor = 'white';
@@ -33,13 +44,17 @@ function App() {
 
   return (
     <>
+    <Router>     
       <Navbar  title='TextUtils' aboutText='About' mode={mode} toggleMode = {toggleMode}/>
       <Alert alert = {alert}/>
       <div className='my-4 container'>
-      {/* <About /> */}
-      <TextForm showAlert={showAlert} heading="Enter the text to analyze" mode={mode}/>
+      <Routes> 
+        <Route path='/' element={<TextForm showAlert={showAlert} heading="Enter the text to analyze" mode={mode} />} />
+        <Route path="/about" element={<About />} />
+      </Routes>
       </div>
-      
+    </Router>
+ 
     </>
   );
 }
--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -16,7 +16,7 @@ export default function Navbar(props) {
                 <a className="nav-link active" aria-current="page" href="/">Home</a>
               </li>
               <li className="nav-item">
-                <a className="nav-link" href="/">{props.aboutText}</a>
+                <a className="nav-link" href="/about">{props.aboutText}</a>
               </li>
             </ul>
             <div className={`form-check form-switch text-${props.mode === 'light'? 'dark': 'light'}`}>
